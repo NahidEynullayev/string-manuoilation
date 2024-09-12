06744fc81012c434075ee4f59e0ff4fa04c2705f
NahidEynullayev <eynullayevnahid18@gmail.com>
2024-09-13 01:14:43 +0400

    test

---
+++ mesq.js
@@ -0,0 +1,101 @@
+// todo   string simple task
+
+// ?  task-1
+
+// let word = `JavaScript`;
+
+// !  1ci usul
+
+// console.log(word.charAt(3));
+// ! 2 ci usul
+// console.log(word.at(3));
+
+// ! 3 cu usul
+// console.log(word[3]);
+
+// ? task-2
+
+// let sentence = "This is a sample sentence.";
+
+// console.log(sentence.indexOf("is"));
+
+// !
+// console.log(sentence.indexOf("is", 3));
+
+// ? task-3
+
+// let sentence = "This is a sample sentence.";
+
+// console.log(sentence.split(" "));
+
+// ? task-4
+
+// let phrase = "Programming is fun!";
+
+// console.log(phrase.slice(0, 11));
+
+// ? task-5
+// let phrase = "Programming is fun!";
+
+// console.log(phrase.slice(-4, -1));
+
+// todo alqoritmik
+
+// ? task-1
+
+// let originalString = "Hello, World!";
+// let arr = originalString.split("");
+// let bos = [];
+// console.log(arr);
+
+// for (let i = arr.length; i >= 0; i--) {
+//   bos.push(arr[i]);
+// }
+// console.log(bos);
+
+// console.log(bos.join(""));
+
+// ? task-2
+
+// let sentence = "javascript is fun";
+
+// let arr = sentence.split("");
+
+// for (let i = 0; i < arr.length; i++) {}
+
+// ? task-3
+
+// let qalinSait = [`a`, `ı`, `o`, `u`];
+
+// let text = `Javascript is aweasome!`;
+// let arr = text.split("");
+
+// let cumledeQalin = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < qalinSait.length; j++) {
+//     if (arr[i] === qalinSait[j]) {
+//       cumledeQalin.push(arr[i]);
+//     }
+//   }
+// }
+// console.log(cumledeQalin);
+
+// ? task-4
+
+// let sentence = "This is a sample sentence width long words.";
+// let arr = sentence.split(" ");
+// let max = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i].length > max) {
+//     max = arr[i].length;
+//   }
+
+//   if (arr[i].length === max) {
+//     var netice = arr[i];
+//   }
+// }
+
+// console.log(max);
+// console.log(netice);
